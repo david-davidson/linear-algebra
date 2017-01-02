5c63bf5bc83dbc4bb4765f99e573ee4e3e8e1b9e
David Davidson <david.davidson@formidable.com>
2017-01-02 10:18:52 -0800

    Move docstrings into main class, not unit tests

--- vector.py
+++ vector.py
@@ -4,51 +4,67 @@ class Vector(object):
   def __init__(self, coords):
     self.coords = tuple(coords)
 
-  # Implicitly called in `print` statements
   def __str__(self):
     return "Coordinates: {}".format(self.coords)
 
-  # Implicitly called in `==` comparisons
   def __eq__(self, other):
     return self.coords == other.coords
 
-  # Implicitly called in `+` operations
   def __add__(self, other):
+    """
+    Adds two Vectors.
+
+    Returns a new Vector formed by adding each item to its counterpart. Implicitly called in `+`
+    operations.
+    """
     return Vector(self._merge_with_transformation(other, lambda x, y: x + y))
 
-  # Implicitly called in `-` operations
   def __sub__(self, other):
+    """
+    Subtracts one Vector from another.
+
+    Returns a new Vector formed by subtracting each item from its
+    counterpart. Implicitly called in `-` operations.
+    """
     return Vector(self._merge_with_transformation(other, lambda x, y: x - y))
 
-  # Merges two lists and applies `transform` to each item pair
   def _merge_with_transformation(self, other, transform):
-    return [transform(x, y) for x, y
-      # ^ list comprehension iterates over list, like `map`
-      in zip(self.coords, other.coords)]
-      # ^ `zip` merges equal-length lists into list of pairs
+    """Merges two lists and applies `transform` to each item pair."""
+    return [transform(x, y) for x, y  # ^ list comprehension iterates over list, like `map`
+      in zip(self.coords, other.coords)]  # ^ `zip` merges equal-length lists into list of pairs
 
-  # Returns coords rounded to requested decimal place
   def get_coords_rounded(self, decimal_places):
+    """Returns coords rounded to requested decimal place."""
     return [round(x, decimal_places) for x in self.coords]
 
-  # Returns scalar muliplication (new Vector) of instance * scale
   def get_scalar_multiply(self, scale):
+    """Accepts a number and returns a new Vector formed by multiplying it by each item."""
     return Vector([item * scale for item in self.coords])
 
-  # Returns length of vector
   def get_magnitude(self):
+    """Returns a Vector's length (magnitude)."""
     return math.sqrt(sum([x**2 for x in self.coords]))
 
-  # Returns direction of vector
   def get_direction(self):
+    """
+    Returns a Vector's direction.
+
+    This is expressed as a Vector equivalent to the original but of magnitude 1: that is, the
+    'unit vector'.
+    """
     return self.get_scalar_multiply(1 / self.get_magnitude())
 
-  # Returns "dot product" of two vectors: each item multiplied by counterpart, then list summed
   def get_dot_product(self, other):
+    """
+    Returns the 'dot product' of two Vectors.
+
+    This is the result of multiplying each item by its counterpart, then summing the list.
+    """
     items_multiplied = self._merge_with_transformation(other, lambda x, y: x * y)
     return sum(items_multiplied)
 
   def get_inner_product(self, other):
+    """Returns, in both degrees and radians, the 'inner product' (angle between) two Vectors."""
     dot_products_multiplied = self.get_dot_product(other)
     magnitudes_multiplied = self.get_magnitude() * other.get_magnitude()
     inner_product_radians = math.acos(dot_products_multiplied / magnitudes_multiplied)
--- vector.test.py
+++ vector.test.py
@@ -4,58 +4,35 @@ from vector import Vector
 class TestVectorMethods(unittest.TestCase):
 
   def test_addition(self): # methods must start with `test` to run
-    """
-    Adds two Vectors: returns a new Vector formed by adding each item to its counterpart.
-    """
     vector1 = Vector([1,2])
     vector2 = Vector([3,4])
     self.assertEqual(vector1 + vector2, Vector([4,6]))
 
   def test_subtraction(self):
-    """
-    Subtracts one Vector from another: returns a new one formed by subtracting each item from its
-    counterpart.
-    """
     vector1 = Vector([1,2])
     vector2 = Vector([3,4])
     self.assertEqual(vector1 - vector2, Vector([-2,-2]))
 
   def test_scalar_multiplication(self):
-    """
-    Accepts a number and returns a new Vector formed by multiplying it by each item.
-    """
     vector = Vector([1,2])
     scale = 10
     self.assertEqual(vector.get_scalar_multiply(10), Vector([10, 20]))
 
   def test_magnitude(self):
-    """
-    Returns a Vector's length (magnitude).
-    """
     vector = Vector([3,4])
     self.assertEqual(vector.get_magnitude(), 5)
 
   def test_direction(self):
-    """
-    Returns a Vector equivalent to the original but of magnitude 1: that is, the 'unit vector'.
-    """
     vector = Vector([1,2])
     unit_vector = vector.get_direction()
     self.assertEqual(unit_vector.get_coords_rounded(3), [0.447, 0.894])
 
   def test_dot_product(self):
-    """
-    Returns the 'dot product' of two Vectors: each item multiplied by its counterpart, then list
-    summed.
-    """
     vector1 = Vector([1,2])
     vector2 = Vector([3,4])
     self.assertEqual(vector1.get_dot_product(vector2), 11)
 
   def test_inner_product(self):
-    """
-    Returns, in both degrees and radians, the 'inner product' (angle between) two Vectors.
-    """
     vector1 = Vector([0,1])
     vector2 = Vector([1,0])
     inner_product = vector1.get_inner_product(vector2)
